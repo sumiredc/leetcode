4ed6db79a83092fc54f4cdc2cc75fad1cd084220
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2023-10-04 18:44:30 +0900

    create : valid parentheses

---
+++ rust/src/020/valid_parentheses.rs
@@ -0,0 +1,66 @@
+/// @see https://leetcode.com/problems/valid-parentheses/
+///
+///
+/// @time_complexity O(N)
+/// @space_complexity O(N)
+///
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack: Vec<String> = vec![];
+        let symbols = make_symbols();
+
+        for c in s.chars() {
+            let str_unit = c.to_string();
+
+            if symbols.contains_key(str_unit.as_str()) {
+                // 閉じカッコ
+
+                // stack が 空なら false
+                if stack.is_empty() {
+                    return false;
+                }
+
+                let pair = symbols.get(str_unit.as_str()).unwrap();
+                if stack.pop().unwrap() != *pair {
+                    return false;
+                }
+            } else {
+                // 開始カッコ
+                stack.push(str_unit);
+            }
+        }
+
+        // stack が 空になっているか検証
+        stack.len() == 0
+    }
+}
+
+fn make_symbols() -> HashMap<String, String> {
+    let mut symbols = HashMap::new();
+    symbols.extend(
+        vec![
+            ("}".to_string(), "{".to_string()),
+            ("]".to_string(), "[".to_string()),
+            (")".to_string(), "(".to_string()),
+        ]
+        .into_iter(),
+    );
+    symbols
+}
+
+pub struct Solution;
+
+use rstest::rstest;
+
+#[rstest]
+#[case("()", true)]
+#[case("()[]{}", true)]
+#[case("(]", false)]
+fn is_valid_test(#[case] s: &str, #[case] expected: bool) {
+    let actual = Solution::is_valid(s.to_string());
+    assert_eq!(actual, expected);
+}
+
+fn main() {}
